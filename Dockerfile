550ee406ad4423359c72c261fe351928117f1e36
Lamp <lamp@owo69.me>
2025-04-11 21:55:00 -0700

    no disable tun
    it works...?

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM alpine
-RUN apk add --no-cache sed
 COPY output/* /usr/local/bin/
 COPY start.sh /
 VOLUME ["/etc/yggdrasil/"]
--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/ash
-
 conf_file=/etc/yggdrasil/yggdrasil.conf
-
 [ -f "${conf_file}" ] || yggdrasil -genconf >${conf_file}
-
-sed -i 's/IfName: auto/IfName: none/g' ${conf_file}
-#sed -i 's/AdminListen: unix:\/\/\/var\/run\/yggdrasil.sock/AdminListen: none/g' ${conf_file}
-#sed -i 's/Listen: \[\]/Listen: \[ "tcp:\/\/0.0.0.0:3434" \]/g' ${conf_file}
-
 exec /usr/local/bin/yggdrasil -useconffile ${conf_file}
